281b75651e940c61ae39b4f1b984d14932010641
Pedro Silva <pedro.silva-dev@hotmail.com>
2024-02-15 11:15:52 -0300

    feat: sidemenu module and submobule control store

--- src/app/modules/chat/components/layout/side-menu/side-menu.component.ts
+++ src/app/modules/chat/components/layout/side-menu/side-menu.component.ts
@@ -2,7 +2,6 @@ import { Component, inject } from '@angular/core';
 import { ChatSetupStore } from '../../../../../store/chat-setup.store';
 import { ChatStore } from '../../../../../store/chat.store';
 import { AppThemeSelectionComponent } from '../../../../@shared/components/app-theme-selection/app-theme-selection.component';
-import { IChatSetupModuleItem } from '../../../interfaces/chat-setup.interface';
 
 @Component({
   standalone: true,
@@ -12,10 +11,13 @@ import { IChatSetupModuleItem } from '../../../interfaces/chat-setup.interface';
   templateUrl: './side-menu.component.html',
 })
 export class SideMenuComponent {
-  private chatStore = inject(ChatStore);
+  public chatStore = inject(ChatStore);
   public chatSetupStore = inject(ChatSetupStore);
 
-  public modules: IChatSetupModuleItem[] = [];
-
   ngOnInit() {}
+
+  public handleSelectSubmodule(moduleId: number, submoduleId: number) {
+    this.chatStore.setModuleId(moduleId);
+    this.chatStore.setSubmoduleId(submoduleId);
+  }
 }
